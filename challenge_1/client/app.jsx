349f6910694845420c3b11485ceb166a5951d896
Jacob <Specter@Jacobs-MacBook-Pro-4.local>
2019-06-14 17:18:44 -0500

    created skeleton, encountering webpack config error, will resolve

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,12 @@
+import React from 'react'
+import ReactDom from 'reactDom'
 
+const App = (props)=>{
+    return(
+    <>
+    {null}
+    </>
+    );
+}
 
+ReactDom.render(<App/>, Document.getElementById('root'));
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './client/app.jsx',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+      path: path.join(__dirname, '/public'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './dist'
+    }
+  };
